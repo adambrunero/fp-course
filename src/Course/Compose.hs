8b3cc518800eb4f6e084740a92ad1ffe46c699c6
Adam Brunero <adam.brunero@gmail.com>
2021-07-20 23:57:16 +1000

    finished compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -13,22 +13,35 @@ import Course.Contravariant
 
 newtype Compose f g a =
   Compose (f (g a)) deriving (Show, Eq)
+-- a data type compose which takes two type constructors f and g and a type a
+--Compose :: (* -> *) -> (* -> *) -> * -> *
+-- this is type constructor, type constructor, type , output type
+-- in the definithion f could be a list, g could be an optional, a could be Bool
+-- the result would be a list of optional books
 
+-- starting the Compose Tutorial 20/7/21
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  --(<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+  (<$>) f (Compose fga) = Compose ((f <$>) <$> fga )
+--    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+-- this is mapping f over g a to get  g b
+-- this is the type signature
+-- (<$>) :: (a -> b) -> Compose f g a -> Compose f g b
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure = Compose . pure . pure
+--    error "todo: Course.Compose pure#instance (Compose f g)"
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
-
+  (<*>) (Compose fgab) (Compose fga) = Compose ( lift2 (<*>) fgab fga) -- this is using the sequentional application 
+--  (<*>) (Compose fgab) (Compose fga) = Compose (((<*>) <$> fgab) <*> fga -- this is the function for lift2 , FMAP followed by apply)
+-- this function maps apply on the g to go from g a to gb
+--    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+-- (<*>) :: k (a -> b) -> k a -> k b
+-- in this case it would be Compose f
+-- (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
